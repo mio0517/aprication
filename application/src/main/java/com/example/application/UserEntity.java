8277fe5103fad8cb4677af213ffd1b95c25fbd4a
satomio <mio.sato@broadleaf.co.jp>
2022-07-11 15:41:50 +0900

    UserRepositoryファイルを追加

---
+++ application/src/main/java/com/example/application/UserEntity.java
@@ -0,0 +1,96 @@
+package com.example.application;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class UserEntity {
+  @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+  private String address;
+  private String email;
+  private Date createDate;
+  private Date updateDate;
+
+  
+  public UserEntity() {
+  }
+
+
+  public UserEntity(Integer id, String name, String address, String email, Date createDate, Date updateDate) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.createDate = createDate;
+    this.updateDate = updateDate;
+  }
+
+
+  public Integer getId() {
+    return id;
+  }
+
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getAddress() {
+    return address;
+  }
+
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+
+  public Date getUpdateDate() {
+    return updateDate;
+  }
+
+
+  public void setUpdateDate(Date updateDate) {
+    this.updateDate = updateDate;
+  }
+
+}
