e22bc1f4f269f18b1c6ca4c434987ad5a026d9ba
satomio <mio.sato@broadleaf.co.jp>
2022-07-12 10:52:07 +0900

    メソッドを追加

--- application/src/main/java/com/example/application/UserRepository.java
+++ application/src/main/java/com/example/application/UserRepository.java
@@ -1,9 +1,19 @@
 package com.example.application;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> { //主キー型式integer
   
+@Query(
+  value ="SELECT * FROM user ORDER BY id DESC",
+  nativeQuery = true
+)  
+
+public List<UserEntity> findAllOrderByidDesc();
 }
